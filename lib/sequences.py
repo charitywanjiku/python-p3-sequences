690d3c8156ccb907b698fb413b655f5b2fb0396a
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-03-02 11:57:44 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def print_fibonacci(length):
-    pass
+    fibonacci = [] 
+    if length > 0:
+        fibonacci.append(0)  
+    if length > 1:
+        fibonacci.append(1) 
+    while len(fibonacci) < length:
+        next_number = fibonacci[-1] + fibonacci[-2]  
+        fibonacci.append(next_number)  
+    print(fibonacci)
